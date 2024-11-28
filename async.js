7a94c600dafecb65916fb34378614c60556d1b7c
Amir Dhahri <amirdhahri01@gmail.com>
2024-11-28 16:47:53 +0100

    An update

---
+++ async.js
@@ -0,0 +1,46 @@
+// function task1(callBack){
+//     setTimeout(() => {
+//         console.log(1);
+//         callBack();
+//     },1000)
+// }
+// function task2(callBack){
+//     setTimeout(() => {
+//         console.log(2);
+//         callBack();
+//     },1000)
+// }
+// function task3(callBack){
+//     setTimeout(() => {
+//         console.log(3);
+//         callBack();
+//     },1000)
+// }
+// function task4(callBack){
+//     setTimeout(() => {
+//         console.log(4);
+//         callBack();
+//     },1000)
+// }
+// function task5(callBack){
+//     setTimeout(() => {
+//         console.log(5);
+//         callBack();
+//     },1000)
+// }
+
+// task1(() => {
+//     task2(() => {
+//         task3(() => {
+//             task4(() => {
+//                 task5(() => {
+//                     console.log("End");
+//                 })
+//             })
+//         })
+//     })
+// })
+
+
+
+
